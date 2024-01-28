8cde2d6e71d3158550564d5a8579acacaf465e35
Giorgia <g.sanvito91@gmail.com>
2024-01-28 08:02:20 -0800

    Add todo Repository

---
+++ src/main/java/com/gio/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.gio.todo.repository;
+
+import com.gio.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
