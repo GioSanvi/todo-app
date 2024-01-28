a9fffab19fd7f25eeeca075086db41c69c3d1d47
Giorgia <g.sanvito91@gmail.com>
2024-01-28 08:50:26 -0800

    Add getTodo method

--- src/main/java/com/gio/todo/service/TodoService.java
+++ src/main/java/com/gio/todo/service/TodoService.java
@@ -5,6 +5,8 @@ import com.gio.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TodoService {
     private final TodoRepository todoRepository;
@@ -18,4 +20,9 @@ public class TodoService {
         todoRepository.save(todo);
 
     }
+
+    public List<Todo> getTodo(){
+        return todoRepository.findAll();
+    }
+
 }
